54b7526add8049d166e3aeaaf38183d831eb2135
Abhay Renjan <abhayrenjan@Abhays-MacBook-Air.local>
2023-05-13 23:56:46 +0530

    login

--- web/src/components/container/login/index.jsx
+++ web/src/components/container/login/index.jsx
@@ -1,8 +1,22 @@
+import { useState } from "react";
+
 const Login = () => {
+    
+    const [email, setEmail] = useState('');
+    const [pass, setPass] = useState('');
+    
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        console.log(email);
+    }
     return (
-        <h1>
-            Login
-        </h1>
+        <form onSubmit={handleSubmit}>
+                <label for="email">email</label>
+                <input value={email} onChange={(e) => setEmail(e.target.value)}type="email" placeholder="Email" id="email" name="email"></input>
+                <label for="password">password</label>
+                <input value={pass} onChange={(e) => setPass(e.target.value)}type="password" placeholder="****" id="password" name="password"></input>
+                <button type="submit">Log In</button>     
+        </form>
     )
 }
 
